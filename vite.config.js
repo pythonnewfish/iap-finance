b4d22af0edf3781d54c4b964ba21847e05b5fa5a
pythonnewfish <lmqlmq123@163.com>
2026-06-24 14:41:39 +0800

    Add GitHub Pages deployment config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import { viteSingleFile } from 'vite-plugin-singlefile'
 
 export default defineConfig({
+  base: '/iap-finance/',
   plugins: [react(), viteSingleFile()],
   server: {
     port: 5173,
